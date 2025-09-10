d06f061bea5fc327adf9043213f881a6f990b968
Jake Lee <leeshaoherng@hotmail.com>
2025-09-10 21:12:47 +0800

    feat: Constraints

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,14 +3,39 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
-type Headers map[string]string
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if ch >= 'A' && ch <= 'Z' ||
+			ch >= 'a' && ch <= 'z' ||
+			ch >= '0' && ch <= '9' {
+			found = true
+		}
+		switch ch {
+		case '!', '#', '$', '%', '&', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+		}
+
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+type Headers struct {
+	headers map[string]string
+}
 
 var rn = []byte("\r\n")
 
-func NewHeaders() Headers {
-	return map[string]string{}
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
 }
 
 // fieldLine, so return: key, value or error
@@ -34,11 +59,19 @@ func parseHeader(fieldLine []byte) (string, string, error) {
 	return string(name), string(value), nil
 }
 
+func (h *Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h *Headers) Set(name, value string) {
+	h.headers[strings.ToLower(name)] = value
+}
+
 // Parsing field lines
 // Field lines look like - field-name ":" OWS field-value OWS
 // Have a field-name, a colon, optional space, field-value, optional white space, and then \r\n
 // Done parsing headers when encounter empty field-line, or a \r\n without any content
-func (h Headers) Parse(data []byte) (int, bool, error) {
+func (h *Headers) Parse(data []byte) (int, bool, error) {
 	read := 0
 	done := false
 
@@ -62,9 +95,13 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 			return 0, false, err
 		}
 
+		if !isToken([]byte(name)) {
+			return 0, false, fmt.Errorf("malformed header name")
+		}
+
 		read += idx + len(rn)
 		// map is already a pointer
-		h[name] = value
+		h.Set(name, value)
 	}
 
 	return read, done, nil
