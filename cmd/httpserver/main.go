c3a8138d14b437ea8f0756d533900fa0995a2217
Jake Lee <leeshaoherng@hotmail.com>
2025-09-12 18:37:34 +0800

    feat: Refactor

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -14,26 +14,60 @@ import (
 
 const port = 42069
 
-func main() {
-	s, err := server.Serve(
-		port,
-		func(w io.Writer, req *request.Request) *server.HandlerError {
-			if req.RequestLine.RequestTarget == "/yourproblem" {
-				return &server.HandlerError{
-					StatusCode: response.StatusBadRequest,
-					Message:    "Your problem is not my problem\n",
-				}
-			} else if req.RequestLine.RequestTarget == "/myproblem" {
-				return &server.HandlerError{
-					StatusCode: response.StatusInternalServerError,
-					Message:    "Woopsie, my bad\n",
-				}
-			} else {
-				w.Write([]byte("All good, frfr\n"))
-			}
+func respond400() []byte {
+	return []byte(`<html>
+  <head>
+    <title>400 Bad Request</title>
+  </head>
+  <body>
+    <h1>Bad Request</h1>
+    <p>Your request honestly kinda sucked.</p>
+  </body>
+</html>`)
+}
+func respond500() []byte {
+	return []byte(`<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>`)
+}
+func respond200() []byte {
+	return []byte(`<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>`)
+}
 
-			return nil
-		})
+func main() {
+	s, err := server.Serve(port, func(w *response.Writer, req *request.Request) {
+		h := response.GetDefaultHeaders(0)
+		body := respond200()
+		status := response.StatusOk
+
+		if req.RequestLine.RequestTarget == "/yourproblem" {
+			body = respond400()
+			status = response.StatusBadRequest
+		} else if req.RequestLine.RequestTarget == "/myproblem" {
+			body = respond500()
+			status = response.StatusInternalServerError
+		}
+
+		h.Replace("Content-length", fmt.Sprintf("%d", len(body)))
+		h.Replace("Content-type", "text/html")
+		w.WriteStatusLine(status)
+		w.WriteHeaders(*h)
+		w.WriteBody(body)
+	})
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"net"
@@ -15,7 +14,7 @@ type HandlerError struct {
 	Message    string
 }
 
-type Handler func(w io.Writer, req *request.Request) *HandlerError
+type Handler func(w *response.Writer, req *request.Request)
 
 type Server struct {
 	Closed  bool
@@ -24,34 +23,15 @@ type Server struct {
 
 func runConnection(s *Server, conn io.ReadWriteCloser) {
 	defer conn.Close()
-	headers := response.GetDefaultHeaders(0)
+	responseWriter := response.NewWriter(conn)
 	r, err := request.RequestFromReader(conn)
 	if err != nil {
-		response.WriteStatusLine(conn, response.StatusBadRequest)
-		response.WriteHeaders(conn, headers)
+		responseWriter.WriteStatusLine(response.StatusBadRequest)
+		responseWriter.WriteHeaders(*response.GetDefaultHeaders(0))
 		return
 	}
 
-	writer := bytes.NewBuffer([]byte{})
-	handlerError := s.Handler(writer, r)
-
-	var body []byte = nil
-	var status response.StatusCode = response.StatusOk
-	if handlerError != nil {
-		status = handlerError.StatusCode
-		body = []byte(handlerError.Message)
-		// return
-	} else {
-		body = writer.Bytes()
-	}
-
-	// Reset Content-length
-	headers.Replace("Content-length", fmt.Sprintf("%d", len(body)))
-
-	// Write status line, headers, and then body
-	response.WriteStatusLine(conn, status)
-	response.WriteHeaders(conn, headers)
-	conn.Write(body)
+	s.Handler(responseWriter, r)
 }
 
 func runServer(s *Server, listener net.Listener) {
